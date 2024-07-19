3ec7cff72dbd750af0bf1fe92b4b34981696f348
Amru2010 <amrutanshjha2010@gmail.com>
2024-07-20 01:13:51 +0530

    Completed the basic setup

---
+++ src/utils/errors/index.js
@@ -0,0 +1,9 @@
+const AppEror = require("./app-error");
+const ServiceError = require("./service-error");
+const ValidationError = require("./validation-error");
+
+module.exports={
+    AppEror,
+    ValidationError,
+    ServiceError
+}
